f9bb33bb32409c7d6d2e0958a8d68988b4a3820e
pall23-mech <142810678+pall23-mech@users.noreply.github.com>
2023-11-28 10:16:41 +0000

    Update BrightestSpot.py
    FPS disp, find from greyscale a bright spot

--- BrightestSpot.py
+++ BrightestSpot.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 
 # Start video capture
 cap = cv2.VideoCapture(0)
@@ -6,6 +7,11 @@ cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     print("Error: Webcam not accessible")
     exit()
+
+# Initialize variables for FPS calculation
+fps = 0
+frame_count = 0
+start_time = time.time()
 
 while True:
     # Capture frame-by-frame
@@ -16,6 +22,16 @@ while True:
         print("Error: Can't receive frame (stream end?). Exiting ...")
         break
 
+    # Increment frame count
+    frame_count += 1
+
+    # Calculate FPS every second
+    current_time = time.time()
+    if current_time - start_time >= 1:
+        fps = frame_count / (current_time - start_time)
+        frame_count = 0
+        start_time = current_time
+
     # Convert to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -24,6 +40,9 @@ while True:
 
     # Mark the brightest spot
     cv2.circle(frame, maxLoc, 10, (255, 0, 0), 2)
+
+    # Display FPS on frame
+    cv2.putText(frame, f'FPS: {fps:.2f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     # Display the resulting frame
     cv2.imshow('Frame with Brightest Spot', frame)
